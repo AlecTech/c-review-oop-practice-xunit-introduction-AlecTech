491d95f06eb29c05941ec296839d463acb2bb70c
AlecTech <eremeevoleg@gmail.com>
2020-10-26 16:42:50 -0600

    added IsYesOrNo method with console read line to test manually first;

--- oct26UnitTestPractice/Program.cs
+++ oct26UnitTestPractice/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Xunit;
-
+using Xunit.Sdk;
 
 namespace oct26UnitTestPractice
 {
@@ -8,21 +8,53 @@ namespace oct26UnitTestPractice
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter yes or no or y or n");
+
+            string x = Console.ReadLine();
+
+            bool y = IsYesOrNo(x);
+            Console.WriteLine($"your input is {y}");
+            //bool b = true;
+           // Console.WriteLine(b.ToString());
+        }
+       
+        
+        public static bool IsYesOrNo(string str)
+        {
+            /*
+             * Write a method called IsYesOrNo that will accept a string and return true if the string is 
+             * “YES”, “Y”, “NO”, or “N”, in any casing, trimmed. Otherwise it will return false
+             *
+             */
+           
+            
+            string input = str.Trim().ToUpper();
+
+            if (input == "YES")
+            {
+                bool myBool = true;
+                return myBool;
+            }
+            else if (input == "Y")
+            {
+                bool myBool = true;
+                return myBool;
+            }
+            else if (input == "NO")
+            {
+                bool myBool = true;
+                return myBool;
+            }
+            else if (input == "N")
+            {
+                bool myBool = true;
+                return myBool;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        public static int DivideByTwo(int number)
-        {
-            return number / 2;
-        }
-
-        public static bool PerfectSquare(int number)
-        {
-            return Math.Sqrt(number) == Math.Ceiling(Math.Sqrt(number));
-        }
-        public static void ThrowsFormatException()
-        {
-            int.Parse("Hello");
-        }
     }
 }
