4d91fff25d58b7bc93290e87d097534e2e2df6ad
AlecTech <eremeevoleg@gmail.com>
2020-10-26 17:29:07 -0600

    xunit test method that will test the conversion to uppercase;

--- oct26UnitTestPractice_Tests/Program_Tests.cs
+++ oct26UnitTestPractice_Tests/Program_Tests.cs
@@ -7,17 +7,34 @@ namespace oct26UnitTestPractice_Tests
     public class Program_Tests
     {
         [Theory]
-        [InlineData(" yes", true)]
-        [InlineData("y ", true)]
-        [InlineData("   no", true)]
-        [InlineData("n   ", true)]
+        [InlineData(" YES", true)]
+        [InlineData("Y ", true)]
+        [InlineData("   NO", true)]
+        [InlineData("N   ", true)]
         [InlineData(" ", false)]
         [InlineData("   ", false)]
         [InlineData("YES", true)]
         [InlineData("NO", true)]
         [InlineData("123", false)]
 
-        public void IsYesOrNo(string input, bool expected)
+        public void IsYesOrNo_Test1(string input, bool expected)
+        {
+            // Arrange - Set up any data, variables, objects, etc necessary for testing.
+            bool output;
+
+            // Act - Call the method, setter, whatever you're testing.
+            output = Program.IsYesOrNo(input);
+
+            // Assert - Check the result to make sure it matches what's expected.
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [InlineData("yes", true)]
+        [InlineData("nO", true)]
+        [InlineData("yes1", false)]
+
+        public void IsYesOrNo_Test2(string input, bool expected)
         {
             // Arrange - Set up any data, variables, objects, etc necessary for testing.
             bool output;
